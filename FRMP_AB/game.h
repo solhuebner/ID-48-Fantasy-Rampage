39462dcd49e556a5f0a6b6780aef27df26fce1a0
trodoss <trodoss@hotmail.com>
2017-02-19 18:14:33 -0600

    Check in of changes
    check in of changes to the code

--- FRMP_AB/game.h
+++ FRMP_AB/game.h
@@ -11,31 +11,35 @@
 #define IN_PLAY_SIZE 2
 #define MAX_ROUNDS 24
 
-#define PLAYER_1 0
-#define PLAYER_2 1
+#define PLAYER_P 0
+#define PLAYER_C 1
 
 #define GAME_MODE_BASIC 0
 #define GAME_MODE_ADVANCED 1
 
-#define GAME_SHOW_DRAW_CARD   0
-#define GAME_SHOW_HAND        1
-#define GAME_SHOW_CARD_PLAYED 2
+#define GAME_SHOW_DRAW_CARD    0
+#define GAME_SHOW_HAND         1
+#define GAME_START_PLAYER_HAND 2
+#define GAME_SHOW_CARD_PLAYED  3
 
 
  char deck[DECK_SIZE];
- char player1_hand[HAND_SIZE];
- char player2_hand[HAND_SIZE];
+ char player_p_hand[HAND_SIZE];
+ char player_c_hand[HAND_SIZE];
  char in_play[IN_PLAY_SIZE];
+
+ char player_p_hand_size = HAND_SIZE;
+ char player_c_hand_size = HAND_SIZE;
 
  char deck_ptr = 0;
  bool card_in_play = false;
  bool deck_drawn = false;
  char drawn_card = -1;
 
- char last_winner = PLAYER_1;
+ char last_winner = PLAYER_P;
  char round_count = 0;
- char player_1_score = 0;
- char player_2_score = 0;
+ char PLAYER_P_score = 0;
+ char PLAYER_C_score = 0;
 
  char hand_ptr = 0;
 
@@ -48,10 +52,18 @@
 CardInfo get_card_info ( char card) {
   int index = (card * 4);
   CardInfo ret;
-  ret.suit = pgm_read_byte_near(card_info_data + index);
-  ret.power = pgm_read_byte_near(card_info_data + (index+1));
-  ret.img = pgm_read_byte_near(card_info_data + (index+2));
-  ret.text = pgm_read_byte_near(card_info_data + (index+3));
+
+  if (card > -1) {
+    ret.suit = pgm_read_byte_near(card_info_data + index);
+    ret.power = pgm_read_byte_near(card_info_data + (index+1));
+    ret.img = pgm_read_byte_near(card_info_data + (index+2));
+    ret.text = pgm_read_byte_near(card_info_data + (index+3));
+  } else {
+    ret.suit = 0;
+    ret.power = 0;
+    ret.img = 0;
+    ret.text = 0;
+  }
   return ret;
 }
 
@@ -91,6 +103,36 @@ char draw_from_deck( char *hand) {
  return draw;
 }
 
+void sort_hand(char *hand) {
+      for(char i=0; i<(HAND_SIZE-1); i++) {
+        for(char o=0; o<(HAND_SIZE-(i+1)); o++) {
+                CardInfo currCard  = get_card_info(hand[o]);
+                CardInfo nextCard  = get_card_info(hand[o+1]);
+
+                //in advanced game mode, if the current suit == the card's 
+                //suit it gets a +1 power boost
+                if (game_mode == GAME_MODE_ADVANCED) {
+                  if (currCard.suit == current_suit) currCard.power++;
+                  if (nextCard.suit == current_suit) nextCard.power++;
+                }
+                
+                if(currCard.power < nextCard.power) {
+                    char t = hand[o];
+                    hand[o] = hand[o+1];
+                    hand[o+1] = t;
+                }
+        }
+    }
+}
+
+char get_hand_size(char *hand) {
+  char hand_size = 0;
+  for(char i=0; i<HAND_SIZE; i++) {
+    if (hand[i] > -1) hand_size++;
+  }
+  return hand_size;
+}
+
 void display_card (char x, char y, char card) {
   char curr_x = x;
   char curr_y = y;
@@ -123,8 +165,8 @@ void start_play_round() {
    memset(in_play,-1,sizeof(in_play));
 
    //players draw cards
-   if (!deck_drawn) drawn_card = draw_from_deck(player1_hand);
-   if (!deck_drawn) draw_from_deck(player2_hand);
+   if (!deck_drawn) drawn_card = draw_from_deck(player_p_hand);
+   if (!deck_drawn) draw_from_deck(player_c_hand);
 
    disp_state = GAME_SHOW_DRAW_CARD;
 }
@@ -137,13 +179,13 @@ void startGame() {
      drawn_card = -1;
      
      //empty hand
-     memset(player1_hand,-1,sizeof(player1_hand));
-     memset(player2_hand,-1,sizeof(player2_hand));
+     memset(player_p_hand,-1,sizeof(player_p_hand));
+     memset(player_c_hand,-1,sizeof(player_c_hand));
 
      //draw initial cards
      for (char i=0; i < 4; i++) {
-      draw_from_deck(player1_hand);
-      draw_from_deck(player2_hand);
+      draw_from_deck(player_p_hand);
+      draw_from_deck(player_c_hand);
      }
 
      start_play_round();
@@ -173,7 +215,14 @@ void stateGameSelectGameMode() {
 void stateShowDrawCard() {
     print_progmem(36, 0, text_your_draw);
     display_card (36,8,drawn_card);
-    if (arduboy.justPressed(A_BUTTON | B_BUTTON)) disp_state = GAME_SHOW_HAND;
+    if (arduboy.justPressed(A_BUTTON | B_BUTTON)) disp_state = GAME_START_PLAYER_HAND;
+}
+
+void stateStartPlayerHand () {
+  hand_ptr = 0;
+  sort_hand(player_p_hand);
+  player_p_hand_size = get_hand_size(player_p_hand);
+  disp_state = GAME_SHOW_HAND;
 }
 
 void stateShowHand () {
@@ -181,19 +230,24 @@ void stateShowHand () {
   
   //draw forward to hand_ptr
   for (char i=0; i < hand_ptr; i++) {
-    display_card((i*8) + 16,8,player1_hand[i]);
+       display_card((i*8) + 16,8,player_p_hand[i]);
   }
-  for (char i=HAND_SIZE-1; i > (hand_ptr-1); i--) {
-    display_card((i*8) + 16,8,player1_hand[i]);
+
+  for (char i=player_p_hand_size-1; i > (hand_ptr-1); i--) {
+       display_card((i*8) + 16,8,player_p_hand[i]);
   }
-  if (arduboy.justPressed(RIGHT_BUTTON) && (hand_ptr < HAND_SIZE)) hand_ptr++;
+  if (arduboy.justPressed(RIGHT_BUTTON) && (hand_ptr < player_p_hand_size)) hand_ptr++;
   if (arduboy.justPressed(LEFT_BUTTON) && (hand_ptr > 0)) hand_ptr--;
-  if (arduboy.justPressed(A_BUTTON | B_BUTTON)) disp_state = GAME_SHOW_CARD_PLAYED;
+  if (arduboy.justPressed(A_BUTTON | B_BUTTON)) {
+    in_play[PLAYER_P] = player_p_hand[hand_ptr];
+    player_p_hand[hand_ptr] = -1;
+    disp_state = GAME_SHOW_CARD_PLAYED;
+  }
 }
 
 void stateShowCardPlayed() {
-    display_card (36,8,player1_hand[hand_ptr]);
-    if (arduboy.justPressed(A_BUTTON | B_BUTTON)) disp_state = GAME_SHOW_HAND;
+    display_card (36,8,in_play[PLAYER_P]);
+    if (arduboy.justPressed(A_BUTTON | B_BUTTON)) disp_state = GAME_START_PLAYER_HAND;
 }
 void stateGamePlaying()
 {
@@ -201,6 +255,10 @@ void stateGamePlaying()
   switch (disp_state) {
     case GAME_SHOW_DRAW_CARD:
        stateShowDrawCard();
+       break;
+
+    case GAME_START_PLAYER_HAND:
+       stateStartPlayerHand();
        break;
        
     case GAME_SHOW_HAND:
