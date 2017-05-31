6655e328b24330b12b8e10c29cb3bdade0b407d8
Joeri Gantois <info@JO3RI.be>
2017-05-31 22:23:05 +0200

    updated the "you lost" song

--- FRMP_AB/song_data.h
+++ FRMP_AB/song_data.h
@@ -186,39 +186,36 @@ Song victory[] = {
 Song lost[] = {
   0x0a,      // Number of tracks
   0x00, 0x00,   // Address of track 0
-  0x0d, 0x00,   // Address of track 1
-  0x14, 0x00,   // Address of track 2
-  0x15, 0x00,   // Address of track 3
-  0x1e, 0x00,   // Address of track 4
-  0x25, 0x00,   // Address of track 5
-  0x2a, 0x00,   // Address of track 6
-  0x3f, 0x00,   // Address of track 7
-  0x42, 0x00,   // Address of track 8
-  0x44, 0x00,   // Address of track 9
+  0x0f, 0x00,   // Address of track 1
+  0x10, 0x00,   // Address of track 2
+  0x11, 0x00,   // Address of track 3
+  0x18, 0x00,   // Address of track 4
+  0x1f, 0x00,   // Address of track 5
+  0x30, 0x00,   // Address of track 6
+  0x45, 0x00,   // Address of track 7
+  0x47, 0x00,   // Address of track 8
+  0x57, 0x00,   // Address of track 9
   0x00,     // Channel 0 entry track
   0x01,     // Channel 1 entry track
   0x02,     // Channel 2 entry track
   0x03,     // Channel 3 entry track
   //"Track channel 0"
-  0x40, 0,    // FX: SET VOLUME: volume = 0
-  0x9D, 25,   // FX: SET TEMPO: tempo = 25
-  0xFC, 7,    // GOTO track 7
   0x40, 48,   // FX: SET VOLUME: volume = 48
+  0x9D, 25,   // FX: SET TEMPO: tempo = 25
+  0x41, -1,   // FX: SLIDE VOLUME ON: -1
+  0xFC, 5,    // GOTO track 5
+  0xFC, 9,    // GOTO track 9
   0x41, -12,    // FX: SLIDE VOLUME ON: -12
   0xFC, 6,    // GOTO track 6
   0x9F,     // FX: STOP CURRENT CHANNEL
   //"Track channel 1"
-  0x40, 48,   // FX: SET VOLUME: volume = 48
-  0x41, -2,   // FX: SLIDE VOLUME ON: -2
-  0xFC, 5,    // GOTO track 5
   0x9F,     // FX: STOP CURRENT CHANNEL
   //"Track channel 2"
   0x9F,     // FX: STOP CURRENT CHANNEL
   //"Track channel 3"
   0x40, 0,    // FX: SET VOLUME: volume = 0
+  0xFC, 7,    // GOTO track 7
   0xFC, 8,    // GOTO track 8
-  0x40, 48,   // FX: SET VOLUME: volume = 48
-  0xFC, 9,    // GOTO track 9
   0x9F,     // FX: STOP CURRENT CHANNEL
   //"Track snare"
   0x40, 32,   // FX: SET VOLUME: volume = 32
@@ -228,9 +225,21 @@ Song lost[] = {
   0xFE,     // RETURN
   //"Track Track 1"
   0x00 + 18,    // NOTE ON: note = 18
-  0x9F + 24,    // DELAY: ticks = 24
-  0x00 + 15,    // NOTE ON: note = 15
-  0x9F + 24,    // DELAY: ticks = 24
+  0x9F + 23,    // DELAY: ticks = 23
+  0x00 + 0,   // NOTE ON: note = 0
+  0x9F + 1,   // DELAY: ticks = 1
+  0x00 + 18,    // NOTE ON: note = 18
+  0x9F + 7,   // DELAY: ticks = 7
+  0x00 + 0,   // NOTE ON: note = 0
+  0x9F + 1,   // DELAY: ticks = 1
+  0x00 + 18,    // NOTE ON: note = 18
+  0x9F + 3,   // DELAY: ticks = 3
+  0x00 + 0,   // NOTE ON: note = 0
+  0x9F + 1,   // DELAY: ticks = 1
+  0x00 + 18,    // NOTE ON: note = 18
+  0x9F + 3,   // DELAY: ticks = 3
+  0x00 + 0,   // NOTE ON: note = 0
+  0x9F + 1,   // DELAY: ticks = 1
   0xFE,     // RETURN
   //"Track Track 2"
   0x00 + 13,    // NOTE ON: note = 13
@@ -255,23 +264,23 @@ Song lost[] = {
   0x9F + 5,   // DELAY: ticks = 5
   0xFE,     // RETURN
   //"Track Track 3"
-  0x00 + 0,   // NOTE ON: note = 0
-  0x9F + 48,    // DELAY: ticks = 48
+  0x9F + 64,    // DELAY: ticks = 64
   0xFE,     // RETURN
   //"Track Track 4"
-  0x9F + 48,    // DELAY: ticks = 48
+  0xFC, 4,    // GOTO track 4
+  0x9F + 2,   // DELAY: ticks = 2
+  0xFC, 4,    // GOTO track 4
+  0x9F + 6,   // DELAY: ticks = 6
+  0xFC, 4,    // GOTO track 4
+  0x9F + 6,   // DELAY: ticks = 6
+  0xFC, 4,    // GOTO track 4
+  0x9F + 2,   // DELAY: ticks = 2
+  0xFC, 4,    // GOTO track 4
+  0x9F + 6,   // DELAY: ticks = 6
   0xFE,     // RETURN
   //"Track Track 5"
-  0xFC, 4,    // GOTO track 4
-  0x9F + 2,   // DELAY: ticks = 2
-  0xFC, 4,    // GOTO track 4
-  0x9F + 6,   // DELAY: ticks = 6
-  0xFC, 4,    // GOTO track 4
-  0x9F + 6,   // DELAY: ticks = 6
-  0xFC, 4,    // GOTO track 4
-  0x9F + 2,   // DELAY: ticks = 2
-  0xFC, 4,    // GOTO track 4
-  0x9F + 6,   // DELAY: ticks = 6
+  0x00 + 13,    // NOTE ON: note = 13
+  0x9F + 24,    // DELAY: ticks = 24
   0xFE,     // RETURN
 };
 
