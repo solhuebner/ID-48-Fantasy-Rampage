3fff3df9950eaa69ed25736a95bea2fa2c1df9f3
Joeri Gantois <info@JO3RI.be>
2017-06-06 21:52:16 +0200

    finished the themeSong with repeating after the intro

--- FRMP_AB/song_data.h
+++ FRMP_AB/song_data.h
@@ -4,84 +4,126 @@
 #define Song const uint8_t PROGMEM
 
 Song themeSong[] = {
-  0x17,                   // Number of tracks
+  0x1B,                   // Number of tracks
   0x00, 0x00,             // Address of track 0
-  0x0C, 0x00,             // Address of track 1
-  0x0D, 0x00,             // Address of track 2
-  0x2D, 0x00,             // Address of track 3
-  0x33, 0x00,             // Address of track 4
-  0x39, 0x00,             // Address of track 5
-  0x42, 0x00,             // Address of track 6
-  0x49, 0x00,             // Address of track 7
-  0x50, 0x00,             // Address of track 8
-  0x53, 0x00,             // Address of track 9
-  0x5E, 0x00,             // Address of track 10
-  0x69, 0x00,             // Address of track 11
-  0x74, 0x00,             // Address of track 12
-  0x7D, 0x00,             // Address of track 13
-  0x8A, 0x00,             // Address of track 14
-  0x97, 0x00,             // Address of track 15
-  0xA2, 0x00,             // Address of track 16
-  0xAF, 0x00,             // Address of track 17
-  0xBC, 0x00,             // Address of track 18
-  0xC9, 0x00,             // Address of track 19
-  0xD8, 0x00,             // Address of track 20
-  0xDB, 0x00,             // Address of track 21
-  0xEB, 0x00,             // Address of track 22
+  0x11, 0x00,             // Address of track 1
+  0x12, 0x00,             // Address of track 2
+  0x32, 0x00,             // Address of track 3
+  0x38, 0x00,             // Address of track 4
+  0x49, 0x00,             // Address of track 5
+  0x4A, 0x00,             // Address of track 6
+  0x67, 0x00,             // Address of track 7
+  0x6B, 0x00,             // Address of track 8
+  0x71, 0x00,             // Address of track 9
+  0x7A, 0x00,             // Address of track 10
+  0x81, 0x00,             // Address of track 11
+  0x88, 0x00,             // Address of track 12
+  0x8B, 0x00,             // Address of track 13
+  0x96, 0x00,             // Address of track 14
+  0xA1, 0x00,             // Address of track 15
+  0xAC, 0x00,             // Address of track 16
+  0xB5, 0x00,             // Address of track 17
+  0xC2, 0x00,             // Address of track 18
+  0xCF, 0x00,             // Address of track 19
+  0xDA, 0x00,             // Address of track 20
+  0xE7, 0x00,             // Address of track 21
+  0xF4, 0x00,             // Address of track 22
+  0x01, 0x01,             // Address of track 23
+  0x10, 0x01,             // Address of track 24
+  0x13, 0x01,             // Address of track 25
+  0x23, 0x01,             // Address of track 26
 
   0x00,                   // Channel 0 entry track
   0x01,                   // Channel 1 entry track
-  0x01,                   // Channel 2 entry track
-  0x01,                   // Channel 3 entry track
+  0x02,                   // Channel 2 entry track
+  0x03,                   // Channel 3 entry track
 
-  //"Track channel 0"
+  //"Intro channel 0"
   0x40, 36,               // FX: SET VOLUME: volume = 36
+  0x9E, 4, 5, 6, 7,       // FX: GOTO advanced: ch0 = 0x04 / ch1 = 0x05 / ch2 = 0x06 / ch3 = 0x07
   0x41, -4,               // FX: SLIDE VOLUME ON: -4
   0x54, 2,                // FX: SET NOTE CUT: 2
   0x9D, 24,               // FX: SET TEMPO: tempo = 24
-  0xFD, 6, 5,             // REPEAT: count = 6 + 1 / track = 5
+  0xFD, 6, 9,             // REPEAT: count = 6 + 1 / track = 9
   0x9F,                   // FX: STOP CURRENT CHANNEL
 
-  //"Track channel 1"
+  //"Intro channel 1"
   0x9F,                   // FX: STOP CURRENT CHANNEL
 
-  //"Track channel 2"
+  //"Intro channel 2"
   0x40, 64,               // FX: SET VOLUME: volume = 64
   0x41, -8,               // FX: SLIDE VOLUME ON: -8
-  0xFD, 1, 20,            // REPEAT: count = 1 + 1 / track = 20
-  0xFC, 9,                // GOTO track 9
-  0xFC, 10,               // GOTO track 10
-  0xFC, 11,               // GOTO track 11
-  0xFC, 12,               // GOTO track 12
+  0xFD, 1, 24,            // REPEAT: count = 1 + 1 / track = 24
   0xFC, 13,               // GOTO track 13
   0xFC, 14,               // GOTO track 14
   0xFC, 15,               // GOTO track 15
-  0xFC, 12,               // GOTO track 12
   0xFC, 16,               // GOTO track 16
   0xFC, 17,               // GOTO track 17
   0xFC, 18,               // GOTO track 18
   0xFC, 19,               // GOTO track 19
-  0x9F,                   // FX: STOP CURRENT CHANNEL
-
-  //"Track channel 3"
+  0xFC, 16,               // GOTO track 16
+  0xFC, 20,               // GOTO track 20
+  0xFC, 21,               // GOTO track 21
   0xFC, 22,               // GOTO track 22
-  0xFD, 12, 21,           // REPEAT: count = 12 + 1 / track = 21
+  0xFC, 23,               // GOTO track 23
   0x9F,                   // FX: STOP CURRENT CHANNEL
 
-  //"Track 4"
+  //"Intro channel 3"
+  0xFC, 26,               // GOTO track 26
+  0xFD, 12, 25,           // REPEAT: count = 12 + 1 / track = 25
+  0x9F,                   // FX: STOP CURRENT CHANNEL
+
+
+
+  //"Track channel 4"
+  0x40, 36,               // FX: SET VOLUME: volume = 36
+  0x9E, 4, 5, 6, 7,       // FX: GOTO advanced: ch0 = 0x04 / ch1 = 0x05 / ch2 = 0x06 / ch3 = 0x07
+  0x41, -4,               // FX: SLIDE VOLUME ON: -4
+  0x54, 2,                // FX: SET NOTE CUT: 2
+  0x9D, 24,               // FX: SET TEMPO: tempo = 24
+  0xFD, 5, 9,             // REPEAT: count = 5 + 1 / track = 9
+  0x9F,                   // FX: STOP CURRENT CHANNEL
+
+  //"Track channel 5"
+  0x9F,                   // FX: STOP CURRENT CHANNEL
+
+  //"Track channel 6"
+  0x40, 64,               // FX: SET VOLUME: volume = 64
+  0x41, -8,               // FX: SLIDE VOLUME ON: -8
+  0xFC, 13,               // GOTO track 13
+  0xFC, 14,               // GOTO track 14
+  0xFC, 15,               // GOTO track 15
+  0xFC, 16,               // GOTO track 16
+  0xFC, 17,               // GOTO track 17
+  0xFC, 18,               // GOTO track 18
+  0xFC, 19,               // GOTO track 19
+  0xFC, 16,               // GOTO track 16
+  0xFC, 20,               // GOTO track 20
+  0xFC, 21,               // GOTO track 21
+  0xFC, 22,               // GOTO track 22
+  0xFC, 23,               // GOTO track 23
+  0x9F,                   // FX: STOP CURRENT CHANNEL
+
+  //"Track channel 7"
+  0xFD, 11, 25,           // REPEAT: count = 12 + 1 / track = 25
+  0x9F,                   // FX: STOP CURRENT CHANNEL
+
+
+
+  //"Track 8"
   0x40, 32,               // FX: SET VOLUME: volume = 32
   0x9F + 1,               // DELAY: ticks = 1
   0x40, 0,                // FX: SET VOLUME: volume = 0
   0xFE,                   // RETURN
 
-  //"Track 5"
-  0xFC, 6,                // GOTO track 6
-  0xFC, 7,                // GOTO track 7
-  0xFC, 6,                // GOTO track 6
-  0xFC, 8,                // GOTO track 8
+  //"Track 9"
+  0xFC, 10,                // GOTO track 10
+  0xFC, 11,                // GOTO track 11
+  0xFC, 10,                // GOTO track 10
+  0xFC, 12,                // GOTO track 12
   0xFE,                   // RETURN
 
-  //"Track 6"
+  //"Track 10"
   0x00 + 4,               // NOTE ON: note = 4
   0x9F + 12,              // DELAY: ticks = 12
   0x00 + 4,               // NOTE ON: note = 4
@@ -90,7 +132,7 @@ Song themeSong[] = {
   0x9F + 12,              // DELAY: ticks = 12
   0xFE,                   // RETURN
 
-  //"Track 7"
+  //"Track 11"
   0x00 + 2,               // NOTE ON: note = 2
   0x9F + 4,               // DELAY: ticks = 4
   0x00 + 2,               // NOTE ON: note = 2
@@ -98,13 +140,13 @@ Song themeSong[] = {
   0x00 + 2,               // NOTE ON: note = 2
   0x9F + 4,               // DELAY: ticks = 4
   0xFE,                   // RETURN
-  
-  //"Track 8"
+
+  //"Track 12"
   0x00 + 7,               // NOTE ON: note = 7
   0x9F + 12,              // DELAY: ticks = 12
   0xFE,                   // RETURN
 
-  //"Track 9"
+  //"Track 13"
   0x00 + 40,              // NOTE ON: note = 40
   0x9F + 12,              // DELAY: ticks = 12
   0x00 + 40,              // NOTE ON: note = 40
@@ -117,7 +159,7 @@ Song themeSong[] = {
   0x9F + 4,               // DELAY: ticks = 4
   0xFE,                   // RETURN
 
-  //"Track 10"
+  //"Track 14"
   0x00 + 47,              // NOTE ON: note = 47
   0x9F + 12,              // DELAY: ticks = 12
   0x00 + 47,              // NOTE ON: note = 47
@@ -130,7 +172,7 @@ Song themeSong[] = {
   0x9F + 4,               // DELAY: ticks = 4
   0xFE,                   // RETURN
 
-  //"Track 11"
+  //"Track 15"
   0x00 + 43,              // NOTE ON: note = 43
   0x9F + 12,              // DELAY: ticks = 12
   0x00 + 43,              // NOTE ON: note = 43
@@ -142,8 +184,8 @@ Song themeSong[] = {
   0x00 + 45,              // NOTE ON: note = 45
   0x9F + 4,               // DELAY: ticks = 4
   0xFE,                   // RETURN
-  
-  //"Track 12"          
+
+  //"Track 16"
   0x00 + 38,              // NOTE ON: note = 38
   0x9F + 12,              // DELAY: ticks = 12
   0x00 + 38,              // NOTE ON: note = 38
@@ -153,8 +195,8 @@ Song themeSong[] = {
   0x00 + 38,              // NOTE ON: note = 38
   0x9F + 12,              // DELAY: ticks = 12
   0xFE,                   // RETURN
-  
-  //"Track 13"
+
+  //"Track 17"
   0x00 + 52,              // NOTE ON: note = 52
   0x9F + 12,              // DELAY: ticks = 12
   0x00 + 52,              // NOTE ON: note = 52
@@ -168,8 +210,8 @@ Song themeSong[] = {
   0x00 + 43,              // NOTE ON: note = 43
   0x9F + 4,               // DELAY: ticks = 4
   0xFE,                   // RETURN
-  
-  //"Track 14"
+
+  //"Track 18"
   0x00 + 45,              // NOTE ON: note = 45
   0x9F + 12,              // DELAY: ticks = 12
   0x00 + 45,              // NOTE ON: note = 45
@@ -183,8 +225,8 @@ Song themeSong[] = {
   0x00 + 40,              // NOTE ON: note = 40
   0x9F + 4,               // DELAY: ticks = 4
   0xFE,                   // RETURN
-  
-  //"Track 15"
+
+  //"Track 19"
   0x00 + 43,              // NOTE ON: note = 43
   0x9F + 12,              // DELAY: ticks = 12
   0x00 + 43,              // NOTE ON: note = 43
@@ -196,8 +238,8 @@ Song themeSong[] = {
   0x00 + 45,              // NOTE ON: note = 45
   0x9F + 4,               // DELAY: ticks = 4
   0xFE,                   // RETURN
-  
-  //"Track 16"
+
+  //"Track 20"
   0x00 + 52,              // NOTE ON: note = 52
   0x9F + 12,              // DELAY: ticks = 12
   0x00 + 52,              // NOTE ON: note = 52
@@ -211,8 +253,8 @@ Song themeSong[] = {
   0x00 + 57,              // NOTE ON: note = 57
   0x9F + 4,               // DELAY: ticks = 4
   0xFE,                   // RETURN
-  
-  //"Track 17"
+
+  //"Track 21"
   0x00 + 59,              // NOTE ON: note = 59
   0x9F + 12,              // DELAY: ticks = 12
   0x00 + 59,              // NOTE ON: note = 59
@@ -226,8 +268,8 @@ Song themeSong[] = {
   0x00 + 55,              // NOTE ON: note = 55
   0x9F + 4,               // DELAY: ticks = 4
   0xFE,                   // RETURN
-  
-  //"Track 18"
+
+  //"Track 22"
   0x00 + 57,              // NOTE ON: note = 57
   0x9F + 12,              // DELAY: ticks = 12
   0x00 + 57,              // NOTE ON: note = 57
@@ -242,7 +284,7 @@ Song themeSong[] = {
   0x9F + 4,               // DELAY: ticks = 4
   0xFE,                   // RETURN
 
-  //"Track 19"
+  //"Track 23"
   0x00 + 52,              // NOTE ON: note = 52
   0x9F + 12,              // DELAY: ticks = 12
   0x40, 36,               // FX: SET VOLUME: volume = 36
@@ -256,25 +298,25 @@ Song themeSong[] = {
   0x9F + 12,              // DELAY: ticks = 12
   0xFE,                   // RETURN
 
-  //"Track 20"
+  //"Track 24"
   0x00 + 0,               // NOTE ON: note = 0
   0x9F + 48,              // DELAY: ticks = 48
   0xFE,                   // RETURN
 
-  //"Track 21"
-  0xFC, 4,                // GOTO track 4
+  //"Track 25"
+  0xFC, 8,                // GOTO track 8
   0x9F + 11,              // DELAY: ticks = 11
-  0xFC, 4,                // GOTO track 4
+  0xFC, 8,                // GOTO track 8
   0x9F + 11,              // DELAY: ticks = 11
-  0xFC, 4,                // GOTO track 4
+  0xFC, 8,                // GOTO track 8
   0x9F + 11,              // DELAY: ticks = 11
-  0xFC, 4,                // GOTO track 4
+  0xFC, 8,                // GOTO track 8
   0x9F + 7,               // DELAY: ticks = 7
-  0xFC, 4,                // GOTO track 4
+  0xFC, 8,                // GOTO track 8
   0x9F + 3,               // DELAY: ticks = 3
   0xFE,                   // RETURN
 
-  //"Track 22"
+  //"Track 26"
   0x9F + 48,              // DELAY: ticks = 48
   0xFE,                   // RETURN
 };
